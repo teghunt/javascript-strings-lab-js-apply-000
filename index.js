ce66650e2d845931e96e6be4f0ddd74e46e860a4
Tom Hunt <teghunt@gmail.com>
2019-01-09 17:57:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var greeting = "";
+var greeting = "hello, everbody";
 
 var specialGuest = "Neil deGrasse Tyson"
 
@@ -7,3 +7,8 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+
+
+
+
+
